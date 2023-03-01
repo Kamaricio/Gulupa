09fac002ebdcf65cf9b6bb7f2ad8b3c826170692
Kamaricio <carlosmauricio41@hotmail.com>
2023-02-28 21:17:15 -0500

    se actualizaron errores

--- JS/Main.js
+++ JS/Main.js
@@ -4,6 +4,21 @@ helados = 8000
 malteadas = 14000
 Postres = 12000
 iva = 1.19
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
+
+
 // Funcion constructora
 class Productos {
     constructor(id, tipo, producto, origen, precio) {
@@ -36,21 +51,19 @@ let pedido = []
 if(localStorage.getItem("pedido")){
     pedido = JSON.parse(localStorage.getItem("pedido"))
 }else{
-    //si no existe, entra al else
-    console.log("Seteamos por primera vez, entra sólo en la primera vez")
     localStorage.setItem("pedido", JSON.stringify(pedido))
 }
 console.log (pedido)
 
-//let pedidoCarrito = JSON.parse(localStorage.getItem("pedido")) || []
 //declaracion al Dom
 let plantilla = document.getElementById("container")
-let carritoproductos = document.getElementById("tarjetaCarrito")
+let carritoproductos = document.getElementById("contenedorPedido")
+let totalCompra = document.getElementById("total")
 
-//let carritoComprar = document.getElementById("tarjetaCarrito")
+//plantilla catalogo de productos
 
 function catalogoProductos(productos) {
-   
+
     for (let fruta of productos) {
         let plantillaCard = document.createElement("div")
         plantillaCard.className = ""
@@ -81,11 +94,10 @@ catalogoProductos(productos)
 class Carrito {
     constructor(producto, cantidad) {
         this.producto = producto,
-            this.cantidad = cantidad
+        this.cantidad = cantidad
     }
     sumaproducto() {
         this.cantidad = this.cantidad + 1
-
     }
 }
 
@@ -96,8 +108,12 @@ function agregarAlCarrito(car) {
 
     if (busquedaCarrito != undefined) {
         let ubicar = pedido.findIndex(e => e.producto == busquedaCarrito.producto)
-        pedido[ubicar].sumaproducto()
+        pedido[ubicar].sumaproducto
         console.log(pedido)
+        Toast.fire({
+            icon: 'success',
+            title: `El producto  se agregó al carrito :3`
+        })
 
     }
     else {
@@ -105,7 +121,10 @@ function agregarAlCarrito(car) {
         pedido.push(agregar)
         console.log(pedido)
         localStorage.setItem("pedido", JSON.stringify (pedido))
-
+        Toast.fire({
+            icon: 'success',
+            title: `El producto se agregó al carrito :3`
+        })
     }
 }
 
@@ -125,13 +144,13 @@ function verCarrito() {
         card.innerHTML = ` <div>
         <div class="infoCarrito">
         <h4>${informacionProducto.producto}</h4>
-        <p>Llevas ${item.cantidad} unidades</p>
+        <p>Llevas  una cantidad de ${item.cantidad}, con un valor de ${informacionProducto.precio} pesos</p>
         </div>
         </div>`
         carritoproductos.append(card)
-
     }
-    
+    compraTotal(pedido)
+
 }
 
 function cerrarCarrito(){
@@ -139,11 +158,56 @@ function cerrarCarrito(){
 }
 function comprar(){
     document.getElementById("mostrarCarrito").style.display = "flex"
-    //document.getElementById("body").style.overflow = "hidden"
-    verCarrito(pedido)
-    //pedido = JSON.parse(localStorage.getItem("pedido"))
 
+    pedido.length >=1 ?  document.getElementById("SinProductos").style.display ="none" : document.getElementById("SinProductos").style.display ="inherit"
+    
+    verCarrito(pedido)
 }
+function compraTotal(pedido){
+    totalCompra.innerHTML = ""
+    let totalPedido = 0
+    
+    for (item of pedido){
+    let informacionProducto = productos.find(fruta => fruta.id === item.producto)
+    totalPedido = totalPedido + informacionProducto.precio
+        totalPedido == 0 ? totalCompra.innerHTML = `No hay productos en el carrito` : totalCompra.innerHTML = `El total del carrito es <strong></strong>`
+        totalCompra.append(totalPedido)
+}
+    
+    // let sumatoria = document.createElement("div")
+    // sumatoria.innerHTML = ``
+}
+
+
+
+
+
+    // let totalCompra = comp.reduce((acc, pedido)=> acc + pedido.precio ,0)
+    // console.log("Acc con reduce " + totalCompra)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Storage y JSON
 //localStorage.setItem("Pedido Guardado", JSON.stringify(pedido))
